5cc9cb8320a693bca54344a22cce256b8ed679bc
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2025-12-08 12:58:06 +0800

    fix profile

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
 const { getProfile } = require("../controllers/userController");
+const { updateProfile } = require("../controllers/userProfileController");
 
 const router = express.Router();
 
