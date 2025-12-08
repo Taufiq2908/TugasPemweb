5e16de0e7a9800854b9e3846e8ef07642c1d5461
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2025-12-08 13:04:21 +0800

    fix profile

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { getProfile } = require("../controllers/userController");
-const { updateProfile } = require("../controllers/userProfileController");
+const { getProfile, updateProfile, getUserPublicProfile } = require("../controllers/userController");
 
 const router = express.Router();
 
-// GET /users/profile → butuh token
 router.get("/profile", protect, getProfile);
 router.put("/profile/update", protect, updateProfile);
-
+router.get("/:id", getUserPublicProfile);
 
 module.exports = router;
