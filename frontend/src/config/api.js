bc6c51ba6949a5b2e7a34c3d90ff5cdb1e53935b
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2025-12-15 21:51:35 +0800

    fix ownerpage

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@
 const LOCAL = "http://localhost:5000";
 
 // Vercel serverless API (nanti kita ganti sesuai domain setelah deploy)
-const PROD = "/api"; 
+const PROD = import.meta.env.VITE_API_URL; 
 
 export const baseURL = import.meta.env.DEV ? LOCAL : PROD;
 
